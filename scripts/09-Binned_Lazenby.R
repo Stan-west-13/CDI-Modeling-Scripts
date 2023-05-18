7964586a93e3eb19f727529a3c78e563b943d85d
Stan-west-13 <swest19@lsu.edu>
2023-05-18 18:10:38 -0500

    change factors TD to NA

--- scripts/09-Binned_Lazenby.R
+++ scripts/09-Binned_Lazenby.R
@@ -5,10 +5,20 @@ View(q)
 library(dplyr)
 
 logit_greaterOne <- q %>%
-    filter(abs(diff_logit) > 1) 
+    filter(abs(diff_logit) > 1 &
+    abs(non_asd_native) >= 20 &
+    abs(non_asd_native) <= 600 &
+    abs(asd_native) >= 20 &
+    abs(asd_native) <= 600) 
 
 
 sig_binBinom_words_log <- sig_binBinom_words %>%
     left_join(., logit_greaterOne) %>%
     arrange(desc(abs(diff_logit))) %>%
     left_join(.,select(cdi_metadata, num_item_id, word))
+
+sig_binBinom_words_ASD_more <- sig_binBinom_words_log %>%
+    filter(diff_logit > 0)
+
+sig_binBinom_words_ASD_less <- sig_binBinom_words_log %>%
+    filter(diff_logit < 0)
