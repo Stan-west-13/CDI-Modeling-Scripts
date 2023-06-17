89f0e6f6d2a6c5e2b8cd901cadb00660125d7fc7
Stan-west-13 <swest19@lsu.edu>
2023-06-17 00:37:38 -0500

    adding code that itteratively deletes words from association network and computes clustering

--- scripts/Exploring_Lazenby_Clustering.R
+++ scripts/Exploring_Lazenby_Clustering.R
@@ -11,8 +11,8 @@ library(tidyr)
 library(ggplot2)
 
 ## Function defining vertex names for obtaining network statistics 
-vertex_names = function (g) {
-    vert = V(g)
+vertex_names <- function (g) {
+    vert <- V(g)
     if (! is.null(names(vert))) names(vert) else as.vector(vert)
 }
 
@@ -63,106 +63,41 @@ clust_laz <- data.frame(word = vertex_names(child_net_graph),
 
 CoxHae_childNodeWise_stats <- rbind(laz_stats, clust_laz)
 
-
+CoxHae_childNodeWise_stats[CoxHae_childNodeWise_stats$word == "soda",]$word <- "soda/pop"
+CoxHae_childNodeWise_stats[CoxHae_childNodeWise_stats$word == "grandma",]$word <- "grandma*" 
+CoxHae_childNodeWise_stats[CoxHae_childNodeWise_stats$word == "turn",]$word <- "turn around" 
 ## Network statistics for significantly different words 
 Lazenby_Nodewise_stats <- CoxHae_childNodeWise_stats %>%
     left_join(., select(cdi_metadata, word, num_item_id)) %>%
     filter(num_item_id %in% words_within_prediction$num_item_id ) %>%
-    arrange(word)
+    arrange(word) %>%
+    pivot_wider(names_from = stat,
+                values_from = value) %>%
+    left_join(., select(words_within_prediction, -word), by = "num_item_id") %>%
+    mutate(ASD_more = ifelse(asd_native > non_asd_native, TRUE, FALSE))
+
+t.test(clustering_coefficient~ASD_more, data = Lazenby_Nodewise_stats %>% filter(asd_native >= 300))
+
+t.test(clustering_coefficient~ASD_more, data = Lazenby_Nodewise_stats)
+t.test(clustering_coefficient~ASD_more, data = Lazenby_Nodewise_stats %>% filter(asd_native <= 200))
 
 
-## Obtaining counts per bin for each word to plot with clustering coefficient 
-participants <- ASD_NA_metadataNetstats_2023_RAN %>%
-    select(group, subjectkey, vocab_size = nProduced, interview_age, sex) %>%
-    unique() %>%
-    mutate(vocab_bin = cut(vocab_size, c(20, seq(50, 600, by = 50)), include.lowest = TRUE))
-
-items <- cdi_metadata %>%
-    select(num_item_id, category, lexical_class, word) %>%
-    arrange(num_item_id) %>%
-    distinct()
-
-d <- ASD_NA_metadataNetstats_2023_RAN %>%
-    filter(Produces) %>%
-    drop_na() %>%
-    select(group, subjectkey, interview_age, vocab_size = nProduced, num_item_id) %>%
-    distinct() %>%
-    mutate(vocab_bin = cut(vocab_size, c(20, seq(50, 600, by = 50)), include.lowest = TRUE))
-
-
-bin_size <- participants %>%
-    group_by(vocab_bin, group) %>%
-    tally( name = "bin_size") %>%
-    pivot_wider(
-        id_cols = "vocab_bin",
-        names_from = "group",
-        names_prefix = "bin_size_",
-        values_from = "bin_size"
-    )
-
-
-x <- d %>%
-    group_by(vocab_bin, group, num_item_id) %>%
-    tally() %>%
-    pivot_wider(
-        id_cols = c(vocab_bin, num_item_id),
-        names_from = group,
-        values_from = n,
-        names_prefix = "n_",
-        values_fill = 0
-    ) %>%
-    pivot_longer(.,
-        cols = c(n_ASD,n_NA),
-        names_to = c("n","group"),
-        names_sep = "_",
-        values_to = "n",
-        names_repair = "unique") %>%
-    left_join(bin_size) %>%
-    ungroup() %>%
-    filter(num_item_id %in% Lazenby_Nodewise_stats$num_item_id) %>%
-    left_join(., Lazenby_Nodewise_stats)
+ggplot(aes(x = non_asd_native, y = clustering_coefficient), data = Lazenby_Nodewise_stats)+
+    geom_point(aes(color = ASD_more))+
+    labs(x = "Vocabulary Size", y = "Clustering Coefficient", color = "ASD more likely to produce")
+    
+x <- lm(clustering_coefficient ~ asd_native * ASD_more, data = Lazenby_Nodewise_stats)
 summary(x)
-
-ggplot(x, aes(x = vocab_bin, y = ))
-
-
-y <- d %>%
-    group_by(vocab_bin, group, num_item_id) %>%
-    tally() %>%
-    pivot_wider(
-        id_cols = c(vocab_bin, num_item_id),
-        names_from = group,
-        values_from = n,
-        names_prefix = "n_",
-        values_fill = 0
-    ) %>%
-    filter(num_item_id %in% Lazenby_Nodewise_stats$num_item_id) %>%
-    left_join(., Lazenby_Nodewise_stats)%>%
-    pivot_wider(c(vocab_bin, num_item_id),
-    names_from = stat,
-    values_from = value) %>%
-    left_join(.,)
+plot(x)
 
 
 
+### "Cooks distance" analysis
+vix <- V(child_net_graph)
 
-logit_greaterOne <- q %>%
-    filter(abs(diff_logit) > 1 &
-    abs(non_asd_native) >= 20 &
-    abs(non_asd_native) <= 600 &
-    abs(asd_native) >= 20 &
-    abs(asd_native) <= 600) 
-
-
-sig_binBinom_words_log <- sig_binBinom_words %>%
-    left_join(., logit_greaterOne) %>%
-    arrange(desc(abs(diff_logit))) %>%
-    left_join(.,select(cdi_metadata, num_item_id, word))
-
-sig_binBinom_words_ASD_more <- sig_binBinom_words_log %>%
-    filter(diff_logit > 0) %>%
-    arrange(desc(abs(diff_native))) 
-
-sig_binBinom_words_ASD_less <- sig_binBinom_words_log %>%
-    filter(diff_logit < 0) %>%
-    arrange(desc(abs(diff_native))) 
+x <- map_dfr(vix, function(v, g) {
+    transitivity(delete_vertices(g, v), type = "global")
+}, g = child_net_graph, .id = .id) %>%
+    t() %>%
+    as.data.frame() %>%
+    mutate(word = rownames(.), diff = transitivity(child_net_graph, type = "global") - V1)
